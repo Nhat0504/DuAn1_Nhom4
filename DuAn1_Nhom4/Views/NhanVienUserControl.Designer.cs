868d96fec1e2b8ab00249c4d287c8eaaf4c5f70c
tainlaph48515 <157346359+tainlaph48515@users.noreply.github.com>
2024-11-13 13:53:30 +0700

    Add files via upload

---
+++ DuAn1_Nhom4/Views/NhanVienUserControl.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace GUI.Views
+{
+    partial class NhanVienUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBox1 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox4 = new CheckBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            groupBox1 = new GroupBox();
+            dataGridView1 = new DataGridView();
+            textBox2 = new TextBox();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(63, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(34, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Tên";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(63, 126);
+            label2.Name = "label2";
+            label2.Size = new Size(72, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Giới Tính";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(63, 198);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Năm Sinh";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(63, 279);
+            label4.Name = "label4";
+            label4.Size = new Size(105, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Số Điện Thoại";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(620, 54);
+            label5.Name = "label5";
+            label5.Size = new Size(58, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Địa Chỉ";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(620, 123);
+            label6.Name = "label6";
+            label6.Size = new Size(47, 20);
+            label6.TabIndex = 5;
+            label6.Text = "Email";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(620, 194);
+            label7.Name = "label7";
+            label7.Size = new Size(83, 20);
+            label7.TabIndex = 6;
+            label7.Text = "Trạng Thái";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(217, 47);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(204, 27);
+            textBox1.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(217, 272);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(204, 27);
+            textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(736, 54);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(250, 27);
+            textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(736, 118);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(250, 27);
+            textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(969, 401);
+            textBox6.Name = "textBox6";
+            textBox6.PlaceholderText = "Tìm Kiếm";
+            textBox6.Size = new Size(267, 27);
+            textBox6.TabIndex = 12;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(201, 122);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(65, 24);
+            checkBox1.TabIndex = 13;
+            checkBox1.Text = "Nam";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(348, 125);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(53, 24);
+            checkBox2.TabIndex = 14;
+            checkBox2.Text = "Nữ";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(747, 194);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(107, 24);
+            checkBox3.TabIndex = 15;
+            checkBox3.Text = "Hoạt Động";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(925, 190);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(157, 24);
+            checkBox4.TabIndex = 16;
+            checkBox4.Text = "Không Hoạt Động";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(141, 360);
+            button1.Name = "button1";
+            button1.Size = new Size(159, 47);
+            button1.TabIndex = 17;
+            button1.Text = "Thêm";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.ForeColor = SystemColors.ControlText;
+            button2.Location = new Point(395, 360);
+            button2.Name = "button2";
+            button2.Size = new Size(174, 47);
+            button2.TabIndex = 18;
+            button2.Text = "Sửa";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.ForeColor = SystemColors.ControlText;
+            button3.Location = new Point(682, 360);
+            button3.Name = "button3";
+            button3.Size = new Size(161, 47);
+            button3.TabIndex = 19;
+            button3.Text = "Xóa";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(dataGridView1);
+            groupBox1.ForeColor = SystemColors.ControlLightLight;
+            groupBox1.Location = new Point(3, 422);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1416, 275);
+            groupBox1.TabIndex = 20;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Thông Tin Nhân Viên Chi Tiết";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(7, 26);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1403, 243);
+            dataGridView1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(217, 192);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(204, 27);
+            textBox2.TabIndex = 21;
+            // 
+            // NhanVienUserControl
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(textBox2);
+            Controls.Add(textBox6);
+            Controls.Add(groupBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(checkBox4);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ForeColor = SystemColors.ControlLightLight;
+            Name = "NhanVienUserControl";
+            Size = new Size(1423, 700);
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox1;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private GroupBox groupBox1;
+        private DataGridView dataGridView1;
+        private TextBox textBox2;
+    }
+}
