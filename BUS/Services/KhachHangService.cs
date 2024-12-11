3faa39dcf5455fc798e0ed6f66ded8e3da023f0b
Nhat0504 <162393654+Nhat0504@users.noreply.github.com>
2024-12-11 11:52:09 +0700

    Add files via upload

---
+++ BUS/Services/KhachHangService.cs
@@ -0,0 +1,55 @@
+﻿using BUS.IServices;
+using DAL.IRepository;
+using DAL.Models;
+using DAL.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS.Services
+{
+    public class KhachHangService : IKhachHangService
+    {
+        private IKhachHangRepos _KHRepos;
+        private List<KhachHang> _lstKH;
+        public KhachHangService()
+        {
+            _KHRepos = new KhachHangRepos();
+            _lstKH = new List<KhachHang>();
+        }
+        public bool Add(KhachHang obj)
+        {
+            _KHRepos.Add(obj);
+            return true;
+        }
+
+        public bool Delete(KhachHang obj)
+        {
+            _KHRepos.Delete(obj);
+            return true;
+        }
+
+        public List<KhachHang> GetAll()
+        {
+            _lstKH = _KHRepos.GetAll();
+            return _lstKH;
+        }
+
+        public List<KhachHang> GetById(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return GetAll();
+            }
+            return _KHRepos.GetAll().Where(c => c.MaKhachHang.ToString().Contains(input.ToLower())).ToList();
+        }
+
+        public bool Update(KhachHang obj)
+        {
+            _KHRepos.Update(obj);
+            return true;
+        }
+    }
+}
