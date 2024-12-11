9040fc4e0ec7e6b9a39a98873574d66ceca7502b
Nhat0504 <162393654+Nhat0504@users.noreply.github.com>
2024-12-11 11:52:49 +0700

    Add files via upload

---
+++ BUS/IServices/IKhachHangService.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS.IServices
+{
+    public interface IKhachHangService
+    {
+        bool Add(KhachHang obj);
+        bool Update(KhachHang obj);
+        bool Delete(KhachHang obj);
+        List<KhachHang> GetAll();
+        List<KhachHang> GetById(string input);
+    }
+}
