5b3b7fde9caee5fa7ba4eb24e157e5ab8bab89e8
Nhat0504 <162393654+Nhat0504@users.noreply.github.com>
2024-12-11 11:50:58 +0700

    Add files via upload

---
+++ DAL/Repository/KhachHangRepos.cs
@@ -0,0 +1,44 @@
+﻿using DAL.IRepository;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+    public class KhachHangRepos : IKhachHangRepos
+    {
+        private QuanLyBanQuanAoContext _dbContext;
+        private List<KhachHang> _lstKH;
+        public KhachHangRepos()
+        {
+            _dbContext = new QuanLyBanQuanAoContext();
+            _lstKH = new List<KhachHang>();
+        }
+        public bool Add(KhachHang obj)
+        {
+            _dbContext.KhachHangs.Add(obj);
+            _dbContext.SaveChanges();
+            return true;
+        }
+        public bool Update(KhachHang obj)
+        {
+            _dbContext.KhachHangs.Update(obj);
+            _dbContext.SaveChanges();
+            return true;
+        }
+        public bool Delete(KhachHang obj)
+        {
+            _dbContext.KhachHangs.Remove(obj);
+            _dbContext.SaveChanges();
+            return true;
+        }
+        public List<KhachHang> GetAll()
+        {
+            _lstKH = _dbContext.KhachHangs.ToList();
+            return _lstKH;
+        }
+    }
+}
